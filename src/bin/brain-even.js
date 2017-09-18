c738eed795d99c8267356115e5ad116136c4e66c
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-09-18 23:37:59 +0300

    add brain-even.js

---
+++ src/bin/brain-even.js
@@ -0,0 +1,6 @@
+import even from '../even';
+
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".');
+console.log('');
+even(Math.floor(Math.random() * 1000));

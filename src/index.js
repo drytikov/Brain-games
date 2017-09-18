fa02295d2e0b1b8358a697eed2e8ca294ec8437c
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-09-18 14:45:02 +0300

    add greeting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
 export default () => {
-  readlineSync.question('May I have your name?: ');
-  console.log('Hi ' + userName + '!');
+  const actual = readlineSync.question('May I have your name?: ');
+  console.log('Hello, ' + actual + '!');
 };

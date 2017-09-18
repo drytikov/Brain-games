d9f23c1b14290e22c6ec8e14cb5ed0a9a750c67b
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-09-19 00:02:45 +0300

    update brain-even.js

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,6 +1,7 @@
+#!/usr/bin/env node
 import even from '../even';
 
 console.log('Welcome to the Brain Games!');
 console.log('Answer "yes" if number even otherwise answer "no".');
 console.log('');
-even(Math.floor(Math.random() * 1000));
+even();

30a173954fd7721c85e1b98992aba4a7d1dfae56
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-09-18 16:42:18 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 
 export default () => {
   const actual = readlineSync.question('May I have your name?: ');
-console.log(`Hello, ${actual}!`);
+  console.log(`Hello, ${actual}!`);
 };

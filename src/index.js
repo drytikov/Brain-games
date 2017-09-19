cfa81ff623ece987e3205c19e6f03a9ef8f0f40d
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-09-19 17:50:55 +0300

    make common interface

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export const random = () => Math.floor(Math.random() * 10);
+export const common = (...param) => {
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
+  console.log('');
+  if (!param) {
+    return;
+  }
+  const iter = (params, index) => {
+    if (params.length === 0) {
+      console.log(`Congratulations, ${name}!`);
+      return;
+    }
+    const [curNumber, correctAnswer, ...rest] = params;
+    console.log(`Question: ${curNumber}`);
+    const curAnswer = readlineSync.question('Your answer: ');
+    if (correctAnswer === curAnswer) {
+      console.log('Correct!');
+      iter(rest, index + 1);
+    } else {
+      console.log(`'${curAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+    }
+  };
+  iter(param, 0);
 };

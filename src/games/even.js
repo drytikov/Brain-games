fdd90976b48442b515c1e9c1120069508bfd0936
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-09-20 22:39:11 +0300

    refactor

--- src/games/even.js
+++ src/games/even.js
@@ -1,11 +1,18 @@
-import { common, random } from '..';
+import { cons } from 'hexlet-pairs';
+import common from '..';
+import random from '../utils';
+
+const gameAbout = 'Answer "yes" if number even otherwise answer "no".';
 
 const isEven = question => question % 2 === 0;
+
 const getCorrectAnswer = question =>
   isEven(question) ? 'yes' : 'no';
-const randQuestion = () => [random()];
-export default () => {
-  const curInfo = 'Answer "yes" if number even otherwise answer "no".';
-  const numOfGames = 3;
-  return common(curInfo, numOfGames, randQuestion, getCorrectAnswer);
+
+const getOptions = () => {
+  const question = random();
+  const correctAnswer = getCorrectAnswer(question);
+  return cons(question, correctAnswer);
 };
+
+export default () => common(gameAbout, getOptions);

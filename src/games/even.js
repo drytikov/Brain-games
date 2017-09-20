f372421dca35a639cec3853cd6790a0e396f362e
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-09-20 15:17:45 +0300

    delete common funcs

--- src/games/even.js
+++ src/games/even.js
@@ -1,24 +1,14 @@
 import { common, random } from '..';
 
-const correctAnswer = (curNumber) => {
-  if (curNumber % 2 === 0) {
+const correctAnswer = (question) => {
+  if (question % 2 === 0) {
     return 'yes';
   }
   return 'no';
 };
+const randQuestion = () => [random()];
 export default () => {
-  console.log('Welcome to the Brain Games!');
-  console.log('Answer "yes" if number even otherwise answer "no".');
-  console.log('');
+  const curInfo = 'Answer "yes" if number even otherwise answer "no".';
   const numOfGames = 3;
-  const iter = (param, index) => {
-    if (index === numOfGames) {
-      common(...param);
-      return;
-    }
-    const number = random();
-    const newParam = param.concat(number, correctAnswer(number));
-    iter(newParam, index + 1);
-  };
-  return iter([], 0);
+  return common(curInfo, numOfGames, randQuestion, correctAnswer);
 };

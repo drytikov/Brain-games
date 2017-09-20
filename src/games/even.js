fba5a522d022b98e7390362ac18c773ab07d77b9
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-09-20 15:40:33 +0300

    fix small bug

--- src/games/even.js
+++ src/games/even.js
@@ -1,14 +1,11 @@
 import { common, random } from '..';
 
-const correctAnswer = (question) => {
-  if (question % 2 === 0) {
-    return 'yes';
-  }
-  return 'no';
-};
+const isEven = question => question % 2 === 0;
+const getCorrectAnswer = question =>
+  isEven(question) ? 'yes' : 'no';
 const randQuestion = () => [random()];
 export default () => {
   const curInfo = 'Answer "yes" if number even otherwise answer "no".';
   const numOfGames = 3;
-  return common(curInfo, numOfGames, randQuestion, correctAnswer);
+  return common(curInfo, numOfGames, randQuestion, getCorrectAnswer);
 };

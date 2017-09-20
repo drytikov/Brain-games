63c35acafbf2912ab89eedf875cd1554896d8863
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-09-20 15:16:17 +0300

    add funcs to common interfase grom games

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
 import readlineSync from 'readline-sync';
 
 export const random = () => Math.floor(Math.random() * 100);
-export const common = (...param) => {
+export const info = about => console.log(about);
+export const common = (curInfo, numOfGames, question, correctAnswer) => {
+  console.log('Welcome to the Brain Games!');
+  info(curInfo);
+  console.log('');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
   console.log('');
-  if (!param) {
-    return;
-  }
-  const iter = (params) => {
-    if (params.length === 0) {
+  const firstQuestion = question();
+  const firstCorrectAnswer = correctAnswer(...firstQuestion);
+  const iter = (curQuestion, curCorrectAnswer, index) => {
+    if (index === numOfGames) {
       console.log(`Congratulations, ${name}!`);
       return;
     }
-    const [curNumber, correctAnswer, ...rest] = params;
-    console.log(`Question: ${curNumber}`);
+    console.log(`Question: ${curQuestion.join(' ')}`);
     const curAnswer = readlineSync.question('Your answer: ');
-    if (correctAnswer === curAnswer) {
+    if (curCorrectAnswer === curAnswer) {
       console.log('Correct!');
-      iter(rest);
+      const nextQuestion = question();
+      const nextCorrectAnswer = correctAnswer(...nextQuestion);
+      iter(nextQuestion, nextCorrectAnswer, index + 1);
     } else {
-      console.log(`'${curAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`'${curAnswer}' is wrong answer ;(. Correct answer was '${curCorrectAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
     }
   };
-  iter(param, 0);
+  iter(firstQuestion, firstCorrectAnswer, 0);
 };

50ee2b2c59984d9ba8a5252a9692952e7dfc0e01
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-09-21 14:39:05 +0300

    style

--- src/games/even.js
+++ src/games/even.js
@@ -1,10 +1,10 @@
 import { cons } from 'hexlet-pairs';
-import common from '..';
+import runGame from '..';
 import random from '../utils';
 
-const gameAbout = 'Answer "yes" if number even otherwise answer "no".';
+const description = 'Answer "yes" if number even otherwise answer "no".';
 
-const isEven = question => question % 2 === 0;
+const isEven = num => num % 2 === 0;
 
 const getCorrectAnswer = question =>
   (isEven(question) ? 'yes' : 'no');
@@ -15,4 +15,4 @@ const getOptions = () => {
   return cons(question, correctAnswer);
 };
 
-export default () => common(gameAbout, getOptions);
+export default () => runGame(description, getOptions);

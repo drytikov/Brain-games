9f4104992990bf60972f37677a941a20de8d0383
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-09-21 22:19:54 +0300

    style

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import { car, cdr } from 'hexlet-pairs';
 
 const numOfGames = 3;
 
-export default (gameAbout, getOptions) => {
+export default (description, getInputParams) => {
   console.log('Welcome to the Brain Games!');
-  console.log(gameAbout);
+  console.log(description);
   console.log('');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
@@ -15,16 +15,16 @@ export default (gameAbout, getOptions) => {
       console.log(`Congratulations, ${name}!`);
       return;
     }
-    const curOptions = getOptions();
-    const curQuestion = car(curOptions);
-    const curCorrectAnswer = cdr(curOptions);
-    console.log(`Question: ${curQuestion}`);
-    const curAnswer = readlineSync.question('Your answer: ');
-    if (curCorrectAnswer === curAnswer) {
+    const inputParams = getInputParams();
+    const question = car(inputParams);
+    const correctAnswer = cdr(inputParams);
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (correctAnswer === answer) {
       console.log('Correct!');
       iter(gameNumber + 1);
     } else {
-      console.log(`'${curAnswer}' is wrong answer ;(. Correct answer was '${curCorrectAnswer}'.`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
     }
   };

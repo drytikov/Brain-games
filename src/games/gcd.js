f31d752e74a9bcbf2a52de5ce03b15c460a53d32
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-09-21 22:19:34 +0300

    style

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,14 +5,16 @@ import random from '../utils';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const getCorrectAnswer = (num1, num2) => {
+const nod = (num1, num2) => {
   if (num2 === 0) {
-    return String(num1);
+    return num1;
   }
-  return getCorrectAnswer(num2, num1 % num2);
+  return nod(num2, num1 % num2);
 };
 
-const getOptions = () => {
+const getCorrectAnswer = (num1, num2) => String(nod(num1, num2));
+
+const getInputParams = () => {
   const num1 = random(1000);
   const num2 = random(1000);
   const question = `${num1} ${num2}`;
@@ -20,4 +22,4 @@ const getOptions = () => {
   return cons(question, correctAnswer);
 };
 
-export default () => runGame(description, getOptions);
+export default () => runGame(description, getInputParams);

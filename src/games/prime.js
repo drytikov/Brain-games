3f2293ba5533679811cc4255742f579fdafef37e
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-09-22 13:55:39 +0300

    refactor

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,21 +4,24 @@ import random from '../utils';
 
 const description = 'Answer "yes" if number prime otherwise answer "no".';
 
-const isPrime = (num, index) => {
+const isPrime = (num) => {
   if (num < 2) {
     return false;
   }
-  if (index > num / 2) {
-    return true;
-  }
-  if (num % index === 0) {
-    return false;
-  }
-  return isPrime(num, index + 1);
+  const iter = (index) => {
+    if (index > num / 2) {
+      return true;
+    }
+    if (num % index === 0) {
+      return false;
+    }
+    return iter(index + 1);
+  };
+  return iter(2);
 };
 
 const getCorrectAnswer = question =>
-  (isPrime(question, 2) ? 'yes' : 'no');
+  (isPrime(question) ? 'yes' : 'no');
 
 const getInputParams = () => {
   const question = random(100);

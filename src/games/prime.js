15f2b4aed9ead3f34b24a4a4f01e5af635479efd
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-09-24 22:21:07 +0300

    update

--- src/games/prime.js
+++ src/games/prime.js
@@ -29,4 +29,4 @@ const getInputParams = () => {
   return cons(question, correctAnswer);
 };
 
-export default name => runGame(name, description, getInputParams);
+export default userName => runGame(userName, description, getInputParams);
--- src/games/progression.js
+++ src/games/progression.js
@@ -5,17 +5,21 @@ import random from '../utils';
 
 const description = 'What number is missing in this progression?';
 const progressionLength = 10;
-const step = random(10);
 
-const getProgression = (acc, numOfItem) => {
-  if (numOfItem === progressionLength) {
-    return acc;
-  }
-  return getProgression([...acc, acc[numOfItem - 1] + step], numOfItem + 1);
+const getProgression = () => {
+  const firstItem = [random(100)];
+  const step = random(10);
+  const iter = (acc, numOfItem) => {
+    if (numOfItem === progressionLength) {
+      return acc;
+    }
+    return iter([...acc, acc[numOfItem - 1] + step], numOfItem + 1);
+  };
+  return iter(firstItem, 1);
 };
 
 const getInputParams = () => {
-  const progression = getProgression([random(100)], 1);
+  const progression = getProgression();
   const numOfRandomItem = random(10);
   const correctAnswer = String(progression[numOfRandomItem]);
   progression[numOfRandomItem] = '...';

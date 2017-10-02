90ab9983c9fd1bcad96aee51e4e59c46dbf1e3c4
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-02 21:26:06 +0300

    update

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,7 +7,7 @@ const description = 'What number is missing in this progression?';
 const progressionLength = 10;
 
 const getProgression = () => {
-  const firstItem = [random(100)];
+  const firstItem = random(100);
   const step = random(10);
   const iter = (acc, numOfItem) => {
     if (numOfItem === progressionLength) {
@@ -15,7 +15,7 @@ const getProgression = () => {
     }
     return iter([...acc, acc[numOfItem - 1] + step], numOfItem + 1);
   };
-  return iter(firstItem, 1);
+  return iter([firstItem], 1);
 };
 
 const getInputParams = () => {
@@ -26,5 +26,4 @@ const getInputParams = () => {
   const question = progression.join(' ');
   return cons(question, correctAnswer);
 };
-
 export default userName => runGame(userName, description, getInputParams);
